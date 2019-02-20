703b2e060002b1747f8d9c625c5b502ef5753eaa
Jonathan Kula <jdkula@stanford.edu>
2019-02-20 15:09:44 -0800

    Add sonar files; begin implementing vmm threads. (not totally sure how I'll proceed with that).

---
+++ PiOS/lib/libpi/sysspec/x86/thread-vmm.c
@@ -0,0 +1,23 @@
+//
+// Created by jonak on 2/20/19.
+//
+
+#include <integer.h>
+
+// TODO: Complete this.
+
+void test_csave() {
+
+}
+
+void rpi_get_cpsr() {
+
+}
+
+void rpi_cswitch(uint32_t **sp_old, uint32_t **sp_new) {
+
+}
+
+void rpi_init_trampoline() {
+
+}
